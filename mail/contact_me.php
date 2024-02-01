fde8cc35bc0cac536c894d01cd37ebeb8fd36cfd
ohenriquet <ohenriquet@gmail.com>
2024-02-01 12:23:12 -0300

    Testes iniciais

---
+++ mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'ohenriquet@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "FORMULARIO DE CONTATO:  $name";
+$email_body = "Nova mensagem de contato.\n\n"."Informações:\n\nNome: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$headers = "From: noreply@agenciacasaamarela.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
